904a40f3358ee3f717fd5b9a06520e6e87ed0a58
matsumoto1199 <114039223+matsumoto1199@users.noreply.github.com>
2022-10-21 16:19:25 +0900

    update1

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace practice2_1021.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string Welcome()
+        {
+            return "This is Welcome page.";
+        }
+        public IActionResult About()
+        {
+            ViewData["message"] = "会社概要ページ";
+            return View();
+        }
+    }
+}
